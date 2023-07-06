c3979af0c458ad346e54613d23e74c5f4dbcb31a
Michal Kleszcz <mkleszcz@apptension.com>
2023-07-06 15:51:07 +0200

    chore: Update discord link

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,5 @@ export const PROJECT_NAME = 'SaaS Boilerplate';
 
 export const DOCS_URL = 'https://docs.demo.saas.apptoku.com/';
 export const LANDING_URL = 'https://www.apptension.com/saas-boilerplate';
-export const DISCORD_URL = 'https://discord.gg/J6bz3BUQ';
+export const DISCORD_URL = 'https://discord.apptension.com';
 export const GITHUB_REPOSITORY = `https://github.com/${GH_REPO_OWNER}/${GH_REPO_NAME}`;
