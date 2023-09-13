7a40f3f38a5c3ab114f0793972a98166a4629650
Michal Kleszcz <mkleszcz@apptension.com>
2023-09-13 17:22:50 +0200

    fix: Disable telemetry on CI

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,5 +8,7 @@ export const LANDING_URL = 'https://www.apptension.com/saas-boilerplate';
 export const DISCORD_URL = 'https://discord.apptension.com';
 export const GITHUB_REPOSITORY = `https://github.com/${GH_REPO_OWNER}/${GH_REPO_NAME}`;
 
-export const SB_TELEMETRY_DISABLED = Boolean(process.env.SB_TELEMETRY_DISABLED) ?? false;
-export const SB_TELEMETRY_DEBUG = Boolean(process.env.SB_TELEMETRY_DEBUG) ?? false;
+const IS_CI = Boolean(process.env.CI ?? false);
+
+export const SB_TELEMETRY_DISABLED = IS_CI || (Boolean(process.env.SB_TELEMETRY_DISABLED) ?? false);
+export const SB_TELEMETRY_DEBUG = IS_CI || (Boolean(process.env.SB_TELEMETRY_DEBUG) ?? false);
